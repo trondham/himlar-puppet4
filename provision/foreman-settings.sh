16825756a2a79108b55893c960bd7493051ea628
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2018-01-15 13:01:46 +0100

    Use full path for repo files in foreman script

--- provision/foreman-settings.sh
+++ provision/foreman-settings.sh
@@ -17,7 +17,7 @@ mgmt_interface=eth0
 mgmt_network=$(facter network_${mgmt_interface})
 mgmt_netmask=$(facter netmask_${mgmt_interface})
 #repo=$(sed -n 's/^baseurl=//p' CentOS-Base.repo | head -1 | rev | cut -d/ -f3- | rev)
-repo=$(sed -n 's/^baseurl=//p' CentOS-Base.repo | head -1)
+repo=$(sed -n 's/^baseurl=//p' /etc/yum.repos.d/CentOS-Base.repo | head -1)
 
 #
 # Location specific configs
